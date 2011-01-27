bcacf2af53519ac3b08747cd6d7091366c0bd383
sjayanti <sjayanti@.invisioninc.local>
2011-01-27 14:33:23 -0500

    Pushing new changes

---
+++ product/nothinbutdotnetstore.specs/infrastructure/DependencyRegistrySpecs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
+using nothinbutdotnetstore.web.infrastructure;
+using Rhino.Mocks;
+
+
+namespace nothinbutdotnetstore.specs.infrastructure
+{
+    public class DependencyRegistrySpecs
+    {
+        public abstract class concern : Observes<DependencyRegistry,
+                                    BasicDependencyRegistry>
+        {
+
+        }
+
+        [Subject(typeof(BasicDependencyRegistry))]
+        public class when_fetching_a_factory : concern
+        {
+
+            Establish e = () =>
+            {
+                dummy_factory = an<DependencyFactory>();
+                var map = new Dictionary<Type, DependencyFactory>();
+                map.Add(typeof(IDummy), dummy_factory);
+                provide_a_basic_sut_constructor_argument(map);
+            };
+
+            Because b = () =>
+                result = sut.get_the_factory_for<IDummy>();
+
+            It should_have_factory = () =>
+            {
+                result.ShouldEqual(dummy_factory);
+            };
+
+            static DependencyFactory result,dummy_factory;
+            
+        }
+        public interface IDummy {}
+        public class Dummy:IDummy
+        {
+            
+        }
+    }
+
+    
+}
---
+++ product/nothinbutdotnetstore/web/infrastructure/BasicDependencyRegistry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using nothinbutdotnetstore.infrastructure.containers;
+
+namespace nothinbutdotnetstore.web.infrastructure
+{
+    public class BasicDependencyRegistry : DependencyRegistry
+    {
+        readonly IDictionary<Type, DependencyFactory> contract_factory_map;
+
+        public BasicDependencyRegistry(IDictionary<Type,DependencyFactory> contract_factory_map)
+        {
+            this.contract_factory_map = contract_factory_map;
+        }
+
+        public DependencyFactory get_the_factory_for<DependencyContract>()
+        {
+           return this.contract_factory_map[typeof(DependencyFactory)];
+        }
+    }
+}
